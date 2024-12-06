ccfed1b18d7d80c37cc3d4dfb563477bbe30b6dc
Pavel Zarubin <p.zarubin@stik.pro>
2024-12-06 09:53:11 +0300

    fix urls

--- src/celery_explorer/urls.py
+++ src/celery_explorer/urls.py
@@ -5,5 +5,5 @@ from celery_explorer.views import check_task_status, get_task_detail, task_index
 urlpatterns = [
     path("task_detail", get_task_detail, name="task_detail"),
     path("check_task_status", check_task_status, name="check_task_status"),
-    path("task_explorer/", task_index, name="task_explorer"),
+    path("", task_index, name="task_explorer"),
 ]
