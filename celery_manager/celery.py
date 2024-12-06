97e55f17437dc93a1330dd297afe89a6772e018f
Pavel Zarubin <p.zarubin@stik.pro>
2024-12-06 10:02:34 +0300

    only app

--- celery_manager/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "celery_manager.settings")
-
-app = Celery("celery_manager")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
